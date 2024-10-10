299f0ccbb8437b5945f38e7813beeafbf0afb3cd
Armoaaa <armoaalaea@gmail.com>
2024-10-10 10:42:39 -0300

    agregar atributos de clase domicilio

--- LogisticaSRL/Models/Domicilio.cs
+++ LogisticaSRL/Models/Domicilio.cs
@@ -7,6 +7,11 @@ namespace LogisticaSRL.Models
 {
     public class Domicilio
     {
-        
+        public int IdDomicilio { get; set; }
+        public string Calle { get; set;}
+        public string Numero { get; set; }
+        public string Ciudad { get; set; }
+        public string Provincia { get; set; }
+        public string CodigoPostal { get; set; }
     }
 }
