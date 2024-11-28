851194318201172b8a664ebb1b5fc260eef761a8
Armoaaa <164027531+Armoaaa@users.noreply.github.com>
2024-11-28 11:09:11 -0300

    migracion

--- LogisticaSRL/Api/Persistencia/Migraciones/20241128140812_InitialMigration.Designer.cs
+++ LogisticaSRL/Api/Persistencia/Migraciones/20241128140812_InitialMigration.Designer.cs
@@ -4,6 +4,7 @@ using Api.Persistencia;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Api.Persistencia.Migraciones
 {
     [DbContext(typeof(GestionPedidoDbContext))]
-    partial class GestionPedidoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241128140812_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
